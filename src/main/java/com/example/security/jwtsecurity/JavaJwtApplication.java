499d2e7d7da61c78e5c3f1adbc9ebe56fd6e45d8
pandubidubidam <pandubidubidam@gmail.com>
2020-06-03 17:37:58 +0700

    java jwt

---
+++ src/main/java/com/example/security/jwtsecurity/JavaJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.jwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJwtApplication.class, args);
+	}
+
+}
